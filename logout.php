2063dd4432eb1404a4400782a148a63aed51489f
Yoanes Panji Prakoso <127073051+Ypanjip@users.noreply.github.com>
2025-04-16 21:36:40 +0700

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,8 +1,18 @@
 <?php
 session_start();
-session_destroy();
+
+// Check if this is the confirmation step (after user clicks "OK")
+if (isset($_GET['confirm']) && $_GET['confirm'] == 'yes') {
+    session_destroy();
+    echo "<script>alert('Selamat anda berhasil logout'); location.href = 'login.php';</script>";
+    exit();
+}
 ?>
 <script type="text/javascript">
-    alert('Selamat anda berhasil logout');
-    location.href = "login.php";
+    if (confirm('Apakah anda ingin logout?')) {
+        // Redirect to the same page with a confirmation parameter
+        location.href = "logout.php?confirm=yes";
+    } else {
+        location.href = "index.php";
+    }
 </script>
